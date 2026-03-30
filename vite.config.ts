7dac551288a8bc02b8a8a68ce4bdab37ad3015c6
Verschoof88 <martijn.verschoof@pit-ko.nl>
2026-03-30 08:40:22 +0200

    Change base path from '/weetwoorden-rad/' to '/spellingoefenen/'

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/weetwoorden-rad/',
+  base: '/spellingoefenen/',
   plugins: [react()],
 })
